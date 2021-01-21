00b397dcfb55b471c78ff84454927bb431e6ffe7
Julius Tens <mail@juliustens.eu>
2021-01-21 05:20:08 +0100

    db: update to latest endpoint

--- profiles/db/index.js
+++ profiles/db/index.js
@@ -5,7 +5,7 @@ const products = require('./products')
 const dbProfile = {
 	locale: 'de-DE',
 	timezone: 'Europe/Berlin',
-	endpoint: 'https://dbahn.demo.hafas.de/streckenagent/restproxy/',
+	endpoint: 'https://dbahn.demo.hafas.de/streckenagent/restproxy/latest/',
 	products: products,
 }
 
